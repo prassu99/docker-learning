2c464ead36c3954e17473293e801b40b9a0d695f
Prasanna kumar Marru <mprasana55@gmail.com>
2025-04-27 19:16:56 -0700

    Airflow

---
+++ 3-docker-airflow/dags/1-hello.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2024, 1, 1),
+}
+
+with DAG(
+    dag_id='hello_data_engineer',
+    default_args=default_args,
+    schedule_interval='@daily',  # run daily
+    catchup=False,
+) as dag:
+    task1 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "Hello, Data Engineer!"'
+    )
+task1
